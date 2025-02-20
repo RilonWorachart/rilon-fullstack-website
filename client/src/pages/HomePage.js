02f593420d351f88d40064576ae94dfb29742acb
Nakorn K <nakorn.kitk@gmail.com>
2025-02-20 20:45:20 +0700

    delete scrollcontect page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import Footer from '../components/Footer'
 import CategorySearch from '../components/CategorySearch'
 import RecommendProductList from '../components/homepage/RecommendProductList'
