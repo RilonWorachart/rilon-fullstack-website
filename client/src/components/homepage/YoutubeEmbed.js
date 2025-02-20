b26b60f8cbf6cfc6b4ed54b7824f62eae72182ac
RilonWorachart <riron.worachartgroup@gmail.com>
2025-02-20 17:27:21 +0700

    adjust

--- client/src/components/homepage/YoutubeEmbed.js
+++ client/src/components/homepage/YoutubeEmbed.js
@@ -5,7 +5,7 @@ function YouTubeEmbed() {
     <div className="flex justify-center item-center py-4">
       <iframe 
         src="https://www.youtube.com/embed/FmiUK2iUI5I" 
-        title="YouTube video player" 
+        title="YouTube video player"
         frameborder="0" 
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
         allowfullscreen
